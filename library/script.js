86e38e6331c3b125dbacf1984f1a2a398868be21
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-10-23 16:42:13 +0300

    library: Connect added books to HTML

--- library/script.js
+++ library/script.js
@@ -3,16 +3,50 @@ const library = document.getElementById("library");
 
 const popUp = document.getElementById("pop-up-bg");
 const closePopUp = document.getElementById("close-button");
-
-// Initial hidden
-popUp.style.display = "none";
-
-addBookButton.addEventListener("click", () => (popUp.style.display = "flex"));
-closePopUp.addEventListener("click", () => (popUp.style.display = "none"));
+const okPopUp = document.getElementById("add-button");
 
 const inputTitle = document.getElementById("input-title");
 const inputAuthor = document.getElementById("input-author");
 const inputHaveRead = document.getElementById("input-have-read");
+
+addBookButton.addEventListener("click", () => (popUp.style.display = "flex"));
+closePopUp.addEventListener("click", () => (popUp.style.display = "none"));
+okPopUp.addEventListener("click", addBook);
+
+let lastId = 0;
+
+function addBook() {
+  const title = inputTitle.value;
+  const author = inputAuthor.value;
+  const readStatus = inputHaveRead.value;
+
+  const curBook = {
+    id: lastId,
+    title: title,
+    author: author,
+    readStatus: readStatus,
+  };
+
+  books.push(curBook);
+
+  lastId++;
+
+  createBookCard(curBook);
+
+  popUp.style.display = "none";
+}
+
+function createBookCard(curBook) {
+  let card = document.createElement("div");
+  card.classList.add("book-card");
+
+  card.innerHTML = `
+    <p>Title: ${curBook.title}</p>
+    <p>Author: ${curBook.author}</p>
+    <p>Status: ${curBook.readStatus == "on" ? "Read" : "Not read"}</p> `;
+
+  library.appendChild(card);
+}
 
 let books = [];
 
