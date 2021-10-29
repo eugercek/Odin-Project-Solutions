9d1ec20b8c151259c63441177ea1eb6fdabd12c3
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-10-29 15:24:46 +0300

    tic-tac-toe: Add marking logic

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,30 +1,61 @@
-const GameBoard = (function () {
-  const board = Array.from(document.getElementsByClassName("cell"));
-  let marks = [];
-
-  for (cell of board) {
-    cell.addEventListener("click", (e) => console.log(e));
-  }
-
-  const resetBoard = () => {
-    for (cell of board) {
-      board.innerText = empty;
-    }
-  };
-
-  const setMarks = (arr) => marks.push(...arr);
-
-  return { resetBoard, setMarks, marks };
-})();
-
 const personFactory = (name, mark) => {
+  const plays = () => (this.currentMark = mark);
   return {
     name,
     mark,
+    plays,
   };
 };
 
-const left = personFactory("You", "X");
-const right = personFactory("You", "O");
+const gameController = (function () {
+  const gameBoard = (function () {
+    const board = _loadCells();
+    let curMark = 0;
 
-GameBoard.setMarks([left.mark, right.mark]);
+    // Private functions
+    function _loadCells() {
+      return Array.from(document.getElementsByClassName("cell"));
+    }
+
+    const _fillCell = (mark, id) => (board[id].innerText = mark);
+
+    const _setEventListeners = function () {
+      for (let cell of board) {
+        cell.addEventListener("click", () => {
+          _fillCell(marks[curMark], cell.getAttribute("data-id"));
+          curMark = curMark == 0 ? 1 : 0;
+        });
+      }
+    };
+
+    // Public functions
+    const clear = () => {
+      for (let cell of board) {
+        cell.innerText = "";
+      }
+    };
+
+    const setMarks = (marks) => (this.marks = marks);
+
+    // Init
+    _loadCells();
+    _setEventListeners();
+
+    return { clear, setMarks };
+  })();
+
+  // Fields
+  const left = personFactory("You", "X");
+  const right = personFactory("Enemy", "O");
+
+  // Private Functions
+  const setMarks = () => gameBoard.setMarks([left, right].map((p) => p.mark));
+
+  // Public Functions
+  const newGame = () => gameBoard.clear();
+
+  // Init
+  setMarks();
+
+  return { newGame };
+})();
