89d632b5aaed10d4d2b9e0cd690bc2821f02a5e3
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-10-29 16:46:36 +0300

    tic-tac-toe: Add game end logic

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -10,22 +10,72 @@ const personFactory = (name, mark) => {
 const gameController = (function () {
   const gameBoard = (function () {
     const board = _loadCells();
-    let curMark = 0;
 
     // Private functions
     function _loadCells() {
       return Array.from(document.getElementsByClassName("cell"));
     }
 
+    const _validCell = (id) => {
+      if (board[id].innerText != "") {
+        return false;
+      }
+
+      return true;
+    };
+
     const _fillCell = (mark, id) => (board[id].innerText = mark);
 
     const _setEventListeners = function () {
       for (let cell of board) {
+        const id = cell.getAttribute("data-id");
         cell.addEventListener("click", () => {
-          _fillCell(marks[curMark], cell.getAttribute("data-id"));
-          curMark = curMark == 0 ? 1 : 0;
+          if (_validCell(id)) {
+            _fillCell(marks[curMark], cell.getAttribute("data-id"));
+            const [gameEnd, type] = isGameEnd();
+            if (gameEnd) {
+              endScreen(type);
+            }
+            nextPlayer();
+          }
         });
       }
+    };
+
+    const isGameEnd = () => {
+      const winnerCombinations = [
+        // By Row
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+
+        // By Column
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+
+        // By Diagonal
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+
+      const curBoard = board.map((e) => e.innerText);
+
+      if (curBoard.every((cell) => cell != "")) {
+        return [true, "draw"];
+      }
+
+      const same = (indexes) =>
+        indexes.every(
+          (i) =>
+            curBoard[i] == marks[curMark] && curBoard[i] == curBoard[indexes[0]]
+        );
+
+      if (winnerCombinations.some((indexes) => same(indexes))) {
+        return [true, marks[curMark]];
+      }
+
+      return [(false, null)];
     };
 
     // Public functions
@@ -35,27 +85,34 @@ const gameController = (function () {
       }
     };
 
-    const setMarks = (marks) => (this.marks = marks);
-
     // Init
     _loadCells();
     _setEventListeners();
 
-    return { clear, setMarks };
+    return { clear };
   })();
 
   // Fields
   const left = personFactory("You", "X");
   const right = personFactory("Enemy", "O");
 
-  // Private Functions
-  const setMarks = () => gameBoard.setMarks([left, right].map((p) => p.mark));
+  const marks = [left, right].map((p) => p.mark);
+  let curMark = 0;
+
+  // Injected Functions
+  const nextPlayer = () => (curMark = curMark == 0 ? 1 : 0);
+  const endScreen = (type) => {
+    if (type == "draw") {
+      console.log("Draw");
+    } else {
+      console.log(`${type} has won!`);
+    }
+  };
 
   // Public Functions
   const newGame = () => gameBoard.clear();
 
   // Init
-  setMarks();
 
   return { newGame };
 })();
