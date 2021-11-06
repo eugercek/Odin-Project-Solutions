111aacb0a65df317bcb9c6c36e111b4269c7c396
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-11-06 18:26:22 +0300

    restaurant: Split webpack into prod/dev and build

---
+++ restaurant/webpack.dev.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app/index.js",
+  devtool: "inline-source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: "./dist",
+  },
+};
+
--- restaurant/webpack.prod.js
+++ restaurant/webpack.prod.js
@@ -1,13 +1,10 @@
 const path = require("path");
+
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/app/index.js",
-  devtool: "inline-source-map",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-  },
-  devServer: {
-    static: "./dist",
   },
 };
