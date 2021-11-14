dd831d7c72b9e821b4463ed3c2d2f6181589ad19
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-11-14 14:34:09 +0300

    library: Add form validation

--- library/script.js
+++ library/script.js
@@ -1,17 +1,20 @@
+const form = document.getElementById("add-form");
+
 const addBookButton = document.getElementById("add-book");
 const library = document.getElementById("library");
 
 const popUp = document.getElementById("pop-up-bg");
 const cancelPopUp = document.getElementById("cancel-add");
-const okPopUp = document.getElementById("add-button");
 
 const inputTitle = document.getElementById("input-title");
 const inputAuthor = document.getElementById("input-author");
+const inputPage = document.getElementById("input-page");
 const inputHaveRead = document.getElementById("input-have-read");
+
+form.addEventListener("submit", addBook);
 
 addBookButton.addEventListener("click", () => (popUp.style.display = "flex"));
 cancelPopUp.addEventListener("click", () => (popUp.style.display = "none"));
-okPopUp.addEventListener("click", addBook);
 
 let lastId = 0;
 let books = [];
@@ -19,13 +22,15 @@ let books = [];
 function addBook() {
   const title = inputTitle.value;
   const author = inputAuthor.value;
+  const page = inputPage.value;
   const readStatus = inputHaveRead.value;
 
   const curBook = {
     id: lastId,
-    title: title,
-    author: author,
-    readStatus: readStatus,
+    title,
+    author,
+    page,
+    readStatus,
   };
 
   books.push(curBook);
@@ -45,9 +50,8 @@ function createBookCard(curBook) {
   card.innerHTML = `
     <div class="card-item">Title: ${curBook.title}</div>
     <div class="card-item">Author: ${curBook.author}</div>
-    <div class="card-item">Status: ${
-      curBook.readStatus == "on" ? "Read" : "Not read"
-    }</div>
+    <div class="card-item">Page: ${curBook.page}</div>
+    <div class="card-item">Status: ${curBook.readStatus}</div>
     <button class="close-button" id="delete-book">x</button>
      `;
 
