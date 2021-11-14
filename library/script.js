b13aec4996f571b7719438d0fb1a6f42ab719f2a
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-11-15 00:46:38 +0300

    library: Finish toolbar functionality
    Really bad code, refactor is on the way!

--- library/script.js
+++ library/script.js
@@ -1,6 +1,9 @@
 const form = document.getElementById("add-form");
 
-const addBookButton = document.getElementById("add-book");
+const toolbarAdd = document.getElementById("add-book");
+const toolbarDelete = document.getElementById("delete-book");
+const toolbarEdit = document.getElementById("edit-book");
+
 const library = document.getElementById("library");
 
 const popUp = document.getElementById("pop-up-bg");
@@ -13,7 +16,10 @@ const inputHaveRead = document.getElementById("input-have-read");
 
 form.addEventListener("submit", addBook);
 
-addBookButton.addEventListener("click", () => (popUp.style.display = "flex"));
+toolbarAdd.addEventListener("click", () => (popUp.style.display = "flex"));
+toolbarDelete.addEventListener("click", () => deleteState());
+toolbarEdit.addEventListener("click", () => editState());
+
 cancelPopUp.addEventListener("click", () => {
   form.reset();
   popUp.style.display = "none";
@@ -59,13 +65,65 @@ function createBookCard(curBook) {
     <div class="card-item page">${curBook.page}</div>
      `;
 
-  // <button class="close-button" id="delete-book">x</button>
   library.appendChild(card);
-  const button = card.getElementsByClassName("close-button")[0];
-  // button.addEventListener("click", () => deleteBook(curBook.id));
 }
 
-function deleteBook(id) {
-  const ele = document.querySelector(`.book-card[data-id="${id}"]`);
-  ele.remove();
+function oneDeletion(book) {
+  book.remove();
+  const bookList = Array.from(document.querySelectorAll(`.book-card`));
+  for (let b of bookList) {
+    b.replaceWith(b.cloneNode(true));
+  }
+}
+
+function deleteState() {
+  const bookList = Array.from(document.querySelectorAll(`.book-card`));
+  for (let book of bookList) {
+    book.addEventListener("click", () => oneDeletion(book));
+  }
+}
+
+function oneEditContent(book) {
+  const haveRead = inputHaveRead.value;
+  book.querySelector(".title").innerText = inputTitle.value;
+  book.querySelector(".author").innerText = inputAuthor.value;
+  book.querySelector(".page").innerText = inputPage.value;
+
+  book.classList.remove("read");
+  book.classList.remove("not-read");
+  book.classList.add(haveRead == "Read" ? "read" : "not-read");
+
+  popUp.style.display = "none";
+  form.removeEventListener("submit", oneEditContent);
+  form.addEventListener("submit", addBook);
+}
+
+function oneEdit(book) {
+  // Load current book's info to form
+  inputTitle.value = book.querySelector(".title").innerText;
+  inputAuthor.value = book.querySelector(".author").innerText;
+  inputPage.value = book.querySelector(".page").innerText;
+  inputHaveRead.value = book.classList.contains("read") ? "Read" : "Not read";
+
+  form.removeEventListener("submit", addBook);
+  form.addEventListener("submit", () => oneEditContent(book));
+
+  popUp.style.display = "flex";
+
+  const bookList = Array.from(document.querySelectorAll(`.book-card`));
+
+  for (let b of bookList) {
+    if (b == book) {
+      continue;
+    }
+
+    b.replaceWith(b.cloneNode(true));
+  }
+}
+
+function editState() {
+  const bookList = Array.from(document.querySelectorAll(`.book-card`));
+  for (let book of bookList) {
+    book.addEventListener("click", () => oneEdit(book));
+  }
 }
