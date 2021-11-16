4553b1870ba1a4cc7558957d3f4ff8e5f4a96b99
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-11-16 23:44:23 +0300

    library: Refactor Library into Library + Book

---
+++ library/Book.js
@@ -0,0 +1,31 @@
+export default class Book {
+  constructor() {
+    this.lastId = 0;
+    this.books = [];
+  }
+
+  addBook({ title, author, page, readStatus }) {
+    this.books.push({ id: this.lastId, title, author, page, readStatus });
+    this.lastId += 1;
+  }
+
+  get lastBook() {
+    return this.books[this.lastId - 1];
+  }
+
+  removeBook(id) {
+    this.books.splice(id, 1);
+  }
+
+  editBook(
+    id,
+    {
+      title = this.books[id],
+      author = this.books[id],
+      page = this.books[id],
+      readStatus = this.books[id],
+    }
+  ) {
+    this.books[id] = { title, author, page, readStatus };
+  }
+}
