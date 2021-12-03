d86d729bd64c13c5fd7a9716b20e641dfc9b00b3
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-12-03 13:37:22 +0300

    weather-app: Add API request

--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -1,1 +1,13 @@
-alert("hi");
+let city = "Ankara";
+let API_KEY = "0e53e87f32b3455f4c4a3fec3c04c53b";
+
+fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`
+)
+  .then((resp) => resp.json())
+  .then(processData);
+
+function processData({ weather }) {
+  const weatherType = weather[0].main;
+  console.log(weatherType);
+}
