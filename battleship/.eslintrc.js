a6d23fabb159377af580e9c1e7cda715d5dce598
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-12-06 15:32:20 +0300

    battleship: Create initial structure

---
+++ battleship/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+
+  plugins: ["prettier"],
+  extends: ["airbnb-base", "prettier"],
+
+  rules: {
+    "prettier/prettier": "warn",
+  },
+};
