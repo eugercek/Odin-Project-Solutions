931049e86279d772d6fbaf5a485b27a7cf50f1de
Emin Umut Gerçek <umutgercek1999@gmail.com>
2022-02-20 04:27:15 +0300

    message-board: Initial commit and index page

---
+++ mini-message-board/routes/index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+module.exports = router;
